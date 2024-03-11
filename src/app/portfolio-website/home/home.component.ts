f83de1539c383a2b976b06ed2b7a3a3c83d5e506
manhhus <seeagen01@gmail.com>
2024-03-11 21:30:19 +0700

    make more beati

--- src/app/portfolio-website/home/home.component.ts
+++ src/app/portfolio-website/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 import {MatCardModule} from '@angular/material/card';
+import { AboutComponent } from '../about/about.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent,MatCardModule],
+  imports: [NavbarComponent,FooterComponent,MatCardModule,AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
